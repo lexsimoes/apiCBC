52acee490137432033ee53493a092b474f54380b
Alexandre Simoes <lexsimoes@yahoo.com>
2023-05-23 14:30:04 -0300

    config criado e configurado no BD

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_HOST', 'localhost'); // Endereço do servidor de banco de dados
+define('DB_USER', 'root'); // Usuário do banco de dados
+define('DB_PASS', ''); // Senha do banco de dados
+define('DB_NAME', 'api1'); // Nome do banco de dados
+
+
+
+?>
